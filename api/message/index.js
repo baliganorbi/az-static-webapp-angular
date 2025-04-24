77b947d7c5d378427a9693cb99ea3b82cbb938d3
Norbert Baliga <baliga.norbert@gmail.com>
2025-04-24 14:11:14 +0200

    Fix string interpolation

--- api/message/index.js
+++ api/message/index.js
@@ -3,8 +3,8 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? '{ "text": "Hello, " + name + ". This HTTP triggered function executed successfully." }'
-        : '{ "text": "Hello World. This HTTP triggered function executed successfully." }';
+        ? `{ "text": "Hello, ${name}. This HTTP triggered function executed successfully." }`
+        : `{ "text": "Hello World. This HTTP triggered function executed successfully." }`;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
